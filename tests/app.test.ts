8d5d5040d0c354066b93153a851addf8332afde1
DCsunset <dcsunset@protonmail.com>
2021-02-08 17:22:02 +0800

    test: add tests for TaskJson API

---
+++ tests/app.test.ts
@@ -0,0 +1,106 @@
+import app from "../src/app";
+import { TaskJson, mergeTaskJson, initTaskJson } from "task.json";
+import * as supertest from "supertest";
+
+// Use agent to persist sessions
+const agent = supertest.agent(app.callback());
+
+describe("TaskJson API", () => {
+	const tj1: TaskJson = {
+		todo: [
+			{
+				uuid: "1",
+				text: "Hello, world 1",
+				start: new Date("2000-01-01").toISOString(),
+				modified: new Date("2010-07-07").toISOString(),
+			},
+			{
+				uuid: "2",
+				text: "Hello, world 2",
+				start: new Date("2000-01-02").toISOString(),
+				modified: new Date("2020-07-07").toISOString()
+			}
+		],
+		done: [],
+		removed: []
+	};
+
+	const tj2: TaskJson = {
+		todo: [
+			{
+				uuid: "1",
+				text: "Hello, world 1 modified",
+				start: new Date("2000-01-01").toISOString(),
+				modified: new Date("2020-07-07").toISOString()
+			}
+		],
+		done: [
+			{
+				uuid: "4",
+				text: "Hello, world 4",
+				start: new Date("2000-01-04").toISOString(),
+				modified: new Date("2020-07-07").toISOString()
+			}
+		],
+		removed: []
+	};
+
+	test("upload TaskJson", async () => {
+		const resp = await agent.put("/")
+			.send(tj1);
+		expect(resp.status).toEqual(200);
+	});
+
+	test("get TaskJson", async () => {
+		const resp = await agent.get("/");
+		expect(resp.status).toEqual(200);
+		expect(resp.body).toEqual(tj1);
+	});
+
+	test("sync TaskJson", async () => {
+		const resp = await agent.patch("/")
+			.send(tj2);
+		expect(resp.status).toEqual(200);
+		expect(resp.body).toEqual(mergeTaskJson(tj1, tj2));
+	});
+
+	test("delete TaskJson", async () => {
+		const resp1 = await agent.delete("/");
+		expect(resp1.status).toEqual(200);
+
+		const resp2 = await agent.get("/");
+		expect(resp2.status).toEqual(200);
+		expect(resp2.body).toEqual(initTaskJson());
+	});
+
+	test("invalid TaskJson", async () => {
+		const tj3 = {
+			todo: [],
+			done: []
+		};
+		const tj4 = {
+			todo: [
+				{
+					uuid: "4",
+					text: "Hello, world 4",
+					start: new Date("2000-01-04").toISOString(),
+				}
+			],
+			done: [],
+			removed: []
+		};
+
+		const resp1 = await agent.put("/")
+			.send(tj3);
+		expect(resp1.status).toEqual(400);
+
+		const resp2 = await agent.patch("/")
+			.send(tj4);
+		expect(resp2.status).toEqual(400);
+
+		// Empty body
+		const resp3 = await agent.patch("/");
+		expect(resp3.status).toEqual(400);
+	});
+});
+

99ffa28b183bfe9737d74bdc309667c5f482464b
DCsunset <dcsunset@protonmail.com>
2021-02-11 21:11:35 +0800

    fix: use rootPath as config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,8 +5,8 @@ export const isProd = process.env.NODE_ENV === 'production';
 export const config = {
 	address: process.env.ADDR || (isProd ? "0.0.0.0" : "localhost"),
 	port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-	dataPath: process.env.DATA_PATH
-		|| (isProd ? "/task.json" : path.join(__dirname, "../../tests/task.json")),
+	rootPath: process.env.ROOT_PATH
+		|| (isProd ? "/data" : path.join(__dirname, "../../tests")),
 	password: process.env.PASSWORD || "admin",
 	maxClients: process.env.MAX_CLIENTS ? parseInt(process.env.MAX_CLIENTS) : 3
 };

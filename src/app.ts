b24b49e8bfbed914632b5420b7c02cb84a957988
DCsunset <DCsunset@protonmail.com>
2023-03-24 16:43:08 -0400

    feat: support lock-free concurrency control

--- src/app.ts
+++ src/app.ts
@@ -35,12 +35,18 @@ fastify.register(fastifyJwt, {
 
 
 // Create a session (log in)
-fastify.post("/session", async (req, reply) => {
-	const { password } = (req.body as any) ?? {};
-	if (!password) {
-		throw new HttpError(400, "Password required");
+fastify.post("/session", {
+	schema: {
+		body: {
+			type: "object",
+			required: ["password"],
+			properties: {
+				password: { type: "string" }
+			}
+		}
 	}
-	
+}, async (req, reply) => {
+	const { password } = req.body as any;
 	if (password === config.password) {
 		const token = fastify.jwt.sign({});
 		reply.send({ token });
@@ -50,30 +56,61 @@ fastify.post("/session", async (req, reply) => {
 	}
 });
 
+// Version of the current taskJson (how many updates)
+// Used for lock-free concurrency control
+const versionBuffer = new SharedArrayBuffer(32)
+const currentVersion = new Uint32Array(versionBuffer);
+currentVersion[0] = 0;
+// max positive number of int32
+const MAX_VERSION = ~(1 << 31);
 
 // Download
 fastify.get("/", async (req, reply) => {
 	await req.jwtVerify();
 	reply.send({
-		data: loadData()
+		data: loadData(),
+		// Digest to detect change when 
+		version: Atomics.load(currentVersion, 0)
 	});
 });
 
 // Upload (can upload encrypted data)
-fastify.put("/", async (req, reply) => {
+fastify.put("/", {
+	schema: {
+		body: {
+			type: "object",
+			required: ["data", "version"],
+			properties: {
+				data: { type: "string" },
+				version: { type: "number" }
+			}
+		}
+	}
+}, async (req, reply) => {
 	await req.jwtVerify();
-	const { data } = (req.body as any) ?? {};
-	if (data) {
+	const { data, version } = req.body as any;
+	if (version === -1) {
+		// -1 means overwriting jexisting data
+		Atomics.add(currentVersion, 0, 1);
+		saveData(data);
+	}
+	else if (Atomics.compareExchange(currentVersion, 0, version, (version+1) % MAX_VERSION) === version) {
+		// Use CAS to ensure only one req saveData when there are concurrent requests with the same version
 		saveData(data);
 	}
 	else {
-		throw new HttpError(400, "Data required");
+		// 409 means conflict
+		reply.code(409).send({
+			// return the new version for client to retry
+			data: loadData()
+		});
 	}
 });
 
 // Delete
-fastify.delete("/", async (req, reply) => {
+fastify.delete("/", async (req, _reply) => {
 	await req.jwtVerify();
+	Atomics.add(currentVersion, 0, 1);
 	deleteData();
 });
 
